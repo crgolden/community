4e0125664ed1bb5ce8c31cbc974ed26c10716cc8
Chris Golden <crgolden@msn.com>
2016-07-03 09:51:55 -0600

    Adding events

--- Data/Migrations/20160703151343_Events.Designer.cs
+++ Data/Migrations/20160703151343_Events.Designer.cs
@@ -8,9 +8,10 @@ using Community.Data;
 namespace Community.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160703151343_Events")]
+    partial class Events
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
---
+++ Models/Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Community.Models
+{
+    public class Event
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public decimal Price { get; set; }
+    }
+}
